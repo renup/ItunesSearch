d30ab629ed6485597fa2e6c23964bcae191d93b3
Renu Punjabi <rpunjabi12@gmail.com>
2020-04-07 18:20:34 -0700

    Cell views in progress

--- ItunesSearch/Views/ItunesCell.swift
+++ ItunesSearch/Views/ItunesCell.swift
@@ -10,7 +10,34 @@ import Foundation
 import UIKit
 
 final class ItunesCell: UITableViewCell, ReusableView {
+    enum Layout {
+        static let high: Float = 1000
+        static let mid: Float = 999
+    }
     
+    private lazy var artworkView: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.contentMode = .center
+        view.setContentHuggingPriority(UILayoutPriority.init(rawValue: Layout.high), for: .horizontal)
+        return view
+    }()
     
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = UIColor.black
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.textAlignment = .left
+        label.setContentHuggingPriority(UILayoutPriority.init(rawValue: Layout.mid), for: .horizontal)
+        return label
+    }()
+    
+    private lazy var subtextLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.textColor = UIColor.gray
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.numberOfLines = 0
+        label.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .horizontal)
+        return label
+    }()
     
 }
